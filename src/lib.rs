3fea7c2e5b543cba6ec2ada551796f8cf69ea151
Vincent Jung <vincent.a.jung@gmail.com>
2024-04-11 14:50:43 +0200

    finished the surface

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use wgpu::{Device, Queue, Surface, SurfaceConfiguration};
+use wgpu::{CommandEncoderDescriptor, Device, Queue, Surface, SurfaceConfiguration};
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
@@ -98,23 +98,64 @@ impl State {
     }
 
     pub fn window(&self) -> &Window {
-        todo!();
+        &self.window
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        todo!();
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
     }
 
-    fn input(&mut self, event: &WindowEvent) -> bool {
-        todo!();
+    fn input(&mut self, _event: &WindowEvent) -> bool {
+        false
     }
 
     fn update(&mut self) {
-        todo!();
+        // todo!();
     }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        todo!();
+        let output = self.surface.get_current_texture()?;
+
+        let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self.device.create_command_encoder(
+            &CommandEncoderDescriptor {
+                label: Some("Render Encoder")
+            }
+        );
+
+        {
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+            drop(_render_pass);
+        }
+
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
 }
 
@@ -158,19 +199,45 @@ pub async fn run() {
         Event::WindowEvent {
             ref event,
             window_id
-        } if window_id == state.window.id() => match event {
-            WindowEvent::CloseRequested |
-            WindowEvent::KeyboardInput {
-                input:
-                KeyboardInput {
-                    state: ElementState::Pressed,
-                    virtual_keycode: Some(VirtualKeyCode::Escape),
+        } if window_id == state.window.id() => if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested |
+                WindowEvent::KeyboardInput {
+                    input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                        ..
+                    },
                     ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
                 },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            _ => {}
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    // new_inner_size is &&mut, so we have to dereference it twice
+                    state.resize(**new_inner_size);
+                }
+                _ => {}
+            }
         },
+        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                // Reconfigure the surface if lost
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                // The system is out of memory, we should probably quit
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                // All other errors (Outdated, Timeout) should be resolved by the next frame
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            // RedrawRequested will only trigger once unless we manually
+            // request it.
+            state.window().request_redraw();
+        }
         _ => {}
     })
 }
